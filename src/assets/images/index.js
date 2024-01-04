b3268017ee62fa10bb8b84e916b6f33a7e4738be
Hani <106341060+Hani0101@users.noreply.github.com>
2024-01-04 03:20:34 +0200

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import geek from './geek.png'
+import gdsc from './gdsc.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    geek,
+    gdsc
+}
