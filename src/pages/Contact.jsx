00858cd4912582024a0e145f61f613afdfbd7954
Hani <106341060+Hani0101@users.noreply.github.com>
2024-02-17 02:50:59 +0200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,9 +30,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Hani",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "hani@gmail.pro",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
